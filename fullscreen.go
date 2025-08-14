ceb0eb62d78a48affd22631470febb9afb17dd20
elazdi-al <alielazdi@gmail.com>
2025-08-14 16:03:00 +0100

    feat: urls get loaded in table

--- fullscreen.go
+++ fullscreen.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
@@ -16,8 +18,16 @@ var baseStyle = lipgloss.NewStyle().
 type model struct {
 	table table.Model
 }
+type row struct {
+	status      string
+	url         string
+	elapsedTime string
+}
 
-func (m model) Init() tea.Cmd { return nil }
+func (m model) Init() tea.Cmd {
+
+	return nil
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
@@ -38,6 +48,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			)
 		}
 	}
+
 	m.table, cmd = m.table.Update(msg)
 	return m, cmd
 }
@@ -46,119 +57,32 @@ func (m model) View() string {
 	return baseStyle.Render(m.table.View()) + "\n"
 }
 
+func load() (*[]table.Row, int) {
+	var file, err = os.ReadFile("urls.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var lines []string = strings.Split(string(file), "\n")
+	var maxLength int = 0
+	var rows []table.Row = []table.Row{}
+	for _, line := range lines {
+		maxLength = utf8.RuneCountInString(line)
+		rows = append(rows, table.Row{"", line, ""})
+	}
+	return &rows, maxLength
+}
+
 func main() {
+	var rows, width = load()
 	columns := []table.Column{
 		{Title: "Status", Width: 6},
-		{Title: "Url", Width: 10},
-		{Title: "Elapsed Time", Width: 10},
-	}
-
-	rows := []table.Row{
-		{"Tokyo", "Japan", "37,274,000"},
-		{"Delhi", "India", "32,065,760"},
-		{"Shanghai", "China", "28,516,904"},
-		{"Dhaka", "Bangladesh", "22,478,116"},
-		{"São Paulo", "Brazil", "22,429,800"},
-		{"Mexico City", "Mexico", "22,085,140"},
-		{"Cairo", "Egypt", "21,750,020"},
-		{"Beijing", "China", "21,333,332"},
-		{"Mumbai", "India", "20,961,472"},
-		{"Osaka", "Japan", "19,059,856"},
-		{"Chongqing", "China", "16,874,740"},
-		{"Karachi", "Pakistan", "16,839,950"},
-		{"Istanbul", "Turkey", "15,636,243"},
-		{"Kinshasa", "DR Congo", "15,628,085"},
-		{"Lagos", "Nigeria", "15,387,639"},
-		{"Buenos Aires", "Argentina", "15,369,919"},
-		{"Kolkata", "India", "15,133,888"},
-		{"Manila", "Philippines", "14,406,059"},
-		{"Tianjin", "China", "14,011,828"},
-		{"Guangzhou", "China", "13,964,637"},
-		{"Rio De Janeiro", "Brazil", "13,634,274"},
-		{"Lahore", "Pakistan", "13,541,764"},
-		{"Bangalore", "India", "13,193,035"},
-		{"Shenzhen", "China", "12,831,330"},
-		{"Moscow", "Russia", "12,640,818"},
-		{"Chennai", "India", "11,503,293"},
-		{"Bogota", "Colombia", "11,344,312"},
-		{"Paris", "France", "11,142,303"},
-		{"Jakarta", "Indonesia", "11,074,811"},
-		{"Lima", "Peru", "11,044,607"},
-		{"Bangkok", "Thailand", "10,899,698"},
-		{"Hyderabad", "India", "10,534,418"},
-		{"Seoul", "South Korea", "9,975,709"},
-		{"Nagoya", "Japan", "9,571,596"},
-		{"London", "United Kingdom", "9,540,576"},
-		{"Chengdu", "China", "9,478,521"},
-		{"Nanjing", "China", "9,429,381"},
-		{"Tehran", "Iran", "9,381,546"},
-		{"Ho Chi Minh City", "Vietnam", "9,077,158"},
-		{"Luanda", "Angola", "8,952,496"},
-		{"Wuhan", "China", "8,591,611"},
-		{"Xi An Shaanxi", "China", "8,537,646"},
-		{"Ahmedabad", "India", "8,450,228"},
-		{"Kuala Lumpur", "Malaysia", "8,419,566"},
-		{"New York City", "United States", "8,177,020"},
-		{"Hangzhou", "China", "8,044,878"},
-		{"Surat", "India", "7,784,276"},
-		{"Suzhou", "China", "7,764,499"},
-		{"Hong Kong", "Hong Kong", "7,643,256"},
-		{"Riyadh", "Saudi Arabia", "7,538,200"},
-		{"Shenyang", "China", "7,527,975"},
-		{"Baghdad", "Iraq", "7,511,920"},
-		{"Dongguan", "China", "7,511,851"},
-		{"Foshan", "China", "7,497,263"},
-		{"Dar Es Salaam", "Tanzania", "7,404,689"},
-		{"Pune", "India", "6,987,077"},
-		{"Santiago", "Chile", "6,856,939"},
-		{"Madrid", "Spain", "6,713,557"},
-		{"Haerbin", "China", "6,665,951"},
-		{"Toronto", "Canada", "6,312,974"},
-		{"Belo Horizonte", "Brazil", "6,194,292"},
-		{"Khartoum", "Sudan", "6,160,327"},
-		{"Johannesburg", "South Africa", "6,065,354"},
-		{"Singapore", "Singapore", "6,039,577"},
-		{"Dalian", "China", "5,930,140"},
-		{"Qingdao", "China", "5,865,232"},
-		{"Zhengzhou", "China", "5,690,312"},
-		{"Ji Nan Shandong", "China", "5,663,015"},
-		{"Barcelona", "Spain", "5,658,472"},
-		{"Saint Petersburg", "Russia", "5,535,556"},
-		{"Abidjan", "Ivory Coast", "5,515,790"},
-		{"Yangon", "Myanmar", "5,514,454"},
-		{"Fukuoka", "Japan", "5,502,591"},
-		{"Alexandria", "Egypt", "5,483,605"},
-		{"Guadalajara", "Mexico", "5,339,583"},
-		{"Ankara", "Turkey", "5,309,690"},
-		{"Chittagong", "Bangladesh", "5,252,842"},
-		{"Addis Ababa", "Ethiopia", "5,227,794"},
-		{"Melbourne", "Australia", "5,150,766"},
-		{"Nairobi", "Kenya", "5,118,844"},
-		{"Hanoi", "Vietnam", "5,067,352"},
-		{"Sydney", "Australia", "5,056,571"},
-		{"Monterrey", "Mexico", "5,036,535"},
-		{"Changsha", "China", "4,809,887"},
-		{"Brasilia", "Brazil", "4,803,877"},
-		{"Cape Town", "South Africa", "4,800,954"},
-		{"Jiddah", "Saudi Arabia", "4,780,740"},
-		{"Urumqi", "China", "4,710,203"},
-		{"Kunming", "China", "4,657,381"},
-		{"Changchun", "China", "4,616,002"},
-		{"Hefei", "China", "4,496,456"},
-		{"Shantou", "China", "4,490,411"},
-		{"Xinbei", "Taiwan", "4,470,672"},
-		{"Kabul", "Afghanistan", "4,457,882"},
-		{"Ningbo", "China", "4,405,292"},
-		{"Tel Aviv", "Israel", "4,343,584"},
-		{"Yaounde", "Cameroon", "4,336,670"},
-		{"Rome", "Italy", "4,297,877"},
-		{"Shijiazhuang", "China", "4,285,135"},
-		{"Montreal", "Canada", "4,276,526"},
+		{Title: "Url", Width: width * 9},
+		{Title: "Elapsed Time", Width: 15},
 	}
 
 	t := table.New(
 		table.WithColumns(columns),
-		table.WithRows(rows),
+		table.WithRows(*rows),
 		table.WithFocused(true),
 		table.WithHeight(7),
 	)
@@ -176,6 +100,7 @@ func main() {
 	t.SetStyles(s)
 
 	m := model{t}
+
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
